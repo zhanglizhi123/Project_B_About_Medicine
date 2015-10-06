4f284f67f299cd696d037cfe0194a28ceea2a6ee
zhanglizhi123 <zhanglizhi111@126.com>
2015-10-06 22:42:48 +0800

    [updata] 10_6 test helpermanager

---
+++ PocketMedicine/ZLZHelperManager.h
@@ -0,0 +1,13 @@
+//
+//  ZLZHelperManager.h
+//  PocketMedicine
+//
+//  Created by lanou3g on 15/10/6.
+//  Copyright © 2015年 organazation. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ZLZHelperManager : NSObject
+
+@end
---
+++ PocketMedicine/ZLZHelperManager.m
@@ -0,0 +1,13 @@
+//
+//  ZLZHelperManager.m
+//  PocketMedicine
+//
+//  Created by lanou3g on 15/10/6.
+//  Copyright © 2015年 organazation. All rights reserved.
+//
+
+#import "ZLZHelperManager.h"
+
+@implementation ZLZHelperManager
+
+@end
